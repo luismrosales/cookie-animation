ddf50db41da154c528c6a99a29623ba6b7f2966b
Luis Rosales <luismrosales2@gmail.com>
2023-02-27 19:58:03 -0500

    finished simple cookie animation

--- app.js
+++ app.js
@@ -13,7 +13,19 @@ tl.fromTo(
   "<50%"
 );
 tl.fromTo(".text", { x: 30, opacity: 0 }, { x: 0, opacity: 1 }, "<");
-
+// cookie jump
+tl.fromTo(
+  ".cookie",
+  { y: 0, rotation: "0deg" },
+  { y: -20, rotation: "-20deg", yoyo: true, repeat: -1 }
+);
+tl.fromTo(
+  "#crumbs",
+  { y: 0, rotation: "0deg" },
+  { y: -20, rotation: "-20deg", yoyo: true, repeat: 2 },
+  "<"
+);
+// fading the cookie out
 button.addEventListener("click", () => {
   gsap.to(".cookie-container", {
     opacity: 0,
